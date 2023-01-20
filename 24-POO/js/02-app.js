f0c2c4afcffffd16d17f30b432376b730507866d
Chava <schavarria13@alumnos.uaq.mx>
2023-01-20 14:32:13 -0600

    Project 24 finished

---
+++ 24-POO/js/02-app.js
@@ -0,0 +1,15 @@
+class Cliente {
+    constructor(nombre, saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`;
+
+    }
+}
+
+const juan = new Cliente("Juan", 400);
+console.log(juan.mostrarInformacion());
+console.log(juan);
