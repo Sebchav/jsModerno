37e3af57c526d086d1ed6c5c14ceb48ba80d262b
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-02-04 14:07:36 -0600

    Project 36 finished

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -19,9 +19,8 @@ function buscarClima(e){
 
         return;
     }
-    
-    
     //Consultar API
+    consultarAPI(ciudad, pais);
 }
 
 function mostrarError(mensaje){
@@ -42,4 +41,92 @@ function mostrarError(mensaje){
             alerta.remove();
         },3000)
     }
+}
+
+function consultarAPI(ciudad, pais){
+    const appId = "5115893a03438fc51034d4d1d80c82a5";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    spinner();
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( datos => {
+
+            limpiarHTML();
+            console.log(datos);
+            if(datos.cod === "404"){
+                mostrarError("Ciudad no encontrada")
+                return;
+            }
+
+            //imprime la respuesta en el html
+            mostrarClima(datos);
+        });
+    console.log(url);
+}
+
+function mostrarClima(datos){
+    const { name, main: {temp, temp_max, temp_min} } = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    const max = kelvinACentigrados(temp_max);
+    const min = kelvinACentigrados(temp_min);
+    
+    const nombreCiudad = document.createElement("p");
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add("font-bold", "text-2xl");
+
+    const actual = document.createElement("p");
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add("font-bold", "text-6xl");
+
+    const tempMaxima = document.createElement("p");
+    tempMaxima.innerHTML = `Max: ${max} &#8451;`;
+    tempMaxima.classList.add("text-xl");
+
+    const tempMinima = document.createElement("p");
+    tempMinima.innerHTML = `Min: ${min} &#8451;`;
+    tempMinima.classList.add("text-xl");
+
+    const resultadoDiv = document.createElement("div");
+    resultadoDiv.classList.add("text-center", "text-white");
+
+    resultadoDiv.appendChild(nombreCiudad);
+    resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMaxima);
+    resultadoDiv.appendChild(tempMinima);
+    
+    resultado.appendChild(resultadoDiv);
+}
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15);
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function spinner(){
+    limpiarHTML();
+    const divSpinner = document.createElement("div");
+    divSpinner.classList.add("sk-fading-circle");
+    
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `
+    resultado.appendChild(divSpinner);
 }
